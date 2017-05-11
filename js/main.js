7f2c9e416897b467c45e9868ef002e9466b64d33
thebackpackway <Julia@thebackpackway.com>
2017-05-11 17:07:44 +0200

    day8 - almost done

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(".quote").onHover(function(){
+  $(".element").typed({
+  strings: ["'It leaves you speechless, then turns you into a storyteller.'", "then this is typed"],
+  typeSpeed: 50,
+  showCursor:false
+  });)
+});
